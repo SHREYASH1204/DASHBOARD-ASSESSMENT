a81c45f12d11068b8006628ea28b2713dc05dbd1
shreyash1204 <156986746+SHREYASH1204@users.noreply.github.com>
2025-12-06 02:59:54 +0530

    sachmein hogaya

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ load_dotenv()
 app = Flask(__name__, static_folder="client/build")
 # Use exact origins (no trailing slash). Add your Vercel URL here.
 CORS(app, origins=[
+    "https://dashboard-admin-hazel.vercel.app/",
     "https://assesment-client.vercel.app",
     "https://dashboard-admin.vercel.app",
     "https://assesment-client-shreyashgupta-9970-shreyash-gupta-s-projects.vercel.app",
