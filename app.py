21fc3d95959d4b73004e2203cc25896686c836e2
shreyash1204 <156986746+SHREYASH1204@users.noreply.github.com>
2025-12-06 02:03:39 +0530

    AB SACHMEIN HOGAYA

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__, static_folder="client/build")
 CORS(app, origins=[
     "https://assesment-client.vercel.app",
     "https://assesment-client-shreyashgupta-9970-shreyash-gupta-s-projects.vercel.app",
-    "https://assesment-client-dgw5abljo-shreyash-gupta-s-projects.vercel.app/",
+    "https://assesment-client-8x0dae56f-shreyash-gupta-s-projects.vercel.app/",
     "http://localhost:3000"])
 
 DATA_FILE = "submissions.json"
